b88e1bf3670ced13a87044f656d6b057f6c49e0a
Joe Heck <heckj@mac.com>
2022-02-15 11:59:41 -0800

    Initial Commit

---
+++ Film3D/Film3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film3DApp.swift
+//  Film3D
+//
+//  Created by Joseph Heck on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Film3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
